227c38ae22b6d33e02046c36817f04496e9cda67
Jacopo <jacopo.canton@gmail.com>
2022-03-15 15:46:16 +0100

    done average plot functions

--- src/plots.py
+++ src/plots.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+import glob
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+#-------------------------------------------------------------------------------
+# Data
+#
+#dbdir  = '/home/pi/repos/raumklima/database/'
+dbdir  = '/Users/jcanton/SynologyDrive/githubStuff/raumklima/database/'
+nsensors = 5
+snames = [
+        'Sala',
+        'Studio',
+        'Camera',
+        'Bagno',
+        'Cucina'
+        ]
+
+#-------------------------------------------------------------------------------
+# Classes
+#
 def Sensor():
     def __init__(self, T=0, RH=0):
         self.t  = T
@@ -23,14 +41,9 @@ def Data():
         self.s4 = sensors[3]
         self.s5 = sensors[4]
 
-snames = [
-        'Sala',
-        'Studio',
-        'Camera',
-        'Bagno',
-        'Cucina'
-        ]
-
+#-------------------------------------------------------------------------------
+# Functions
+#
 def readDB(fpath):
 
     # read database
@@ -42,7 +55,10 @@ def readDB(fpath):
     ifile.close()
 
     # convert data
-    nsensors = len(rows[0])-1
+    Tnsensors = len(rows[0])-1
+    if Tnsensors != nsensors:
+        raise ValueError('Number of sensors in table ({}) != from module nsensors ({}).'.format(Tnsensors, nsensors))
+    #
     table = np.zeros(2*nsensors+1)
     for row in rows:
         tstamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
@@ -55,19 +71,15 @@ def readDB(fpath):
 
     table = np.delete(table, (0), axis=0)
 
-    return table, nsensors
+    return table
 
-def do24hrsPlot(dbdir, fpath):
+def doPlot(table, nback=0, figName='fig.png'):
 
-    table, nsensors = readDB(fpath)
-
-    # Plot last 24 hrs
-    nback = 24*60
     fig = plt.figure(1); fig.clear()
     fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, num=fig.number)
     for s in range(nsensors):
-        ax[0].plot(table[-nback:, 0], table[-nback:, 1+2*s]  )
-        ax[1].plot(table[-nback:, 0], table[-nback:, 1+2*s+1], label=snames[s])
+        ax[0].plot(table[nback:, 0], table[nback:, 1+2*s]  )
+        ax[1].plot(table[nback:, 0], table[nback:, 1+2*s+1], label=snames[s])
     ax[1].xaxis.set_major_formatter(
                 mdates.ConciseDateFormatter(ax[1].xaxis.get_major_locator()))
     # Shrink current axis's height by 10% on the bottom
@@ -80,31 +92,63 @@ def do24hrsPlot(dbdir, fpath):
     ax[0].set_ylabel('Temp')
     ax[1].set_ylabel('RH')
     #ax[1].set_xlabel('Time')
-    ax[1].set_xlim([table[max(-nback, -table.shape[0]),0], table[-1,0]])
-    ax[1].set_ylim([30, 55])
+    ax[1].set_xlim([table[max(nback, -table.shape[0]),0], table[-1,0]])
+    ax[1].set_ylim([30, 60])
     ax[0].grid(visible=True, which='major', axis='y', alpha=0.3)
     ax[1].grid(visible=True, which='major', axis='y', alpha=0.3)
     plt.draw()
-    plt.savefig(dbdir + '24hrs.png', dpi=300, bbox_inches='tight')
+    plt.savefig(dbdir + figName, dpi=300, bbox_inches='tight')
     #pickle.dump(fig, open(dbdir + '24hrs.fig.pickle', 'wb'))
     #print('saved 24hrs')
 
-def doAvgPlot(dbdir, fpath):
+#plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+#plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+#plt.plot(x,y)
+#plt.gcf().autofmt_xdate()
 
-def read_and_plot(plotKind='24hr'):
+def doAvgPlot(fpath, ndays=1):
 
-    dbdir  = '/home/pi/repos/raumklima/database/'
+    # get files
+    dbFiles = glob.glob(fpath+'*.csv')
+    dbFiles.sort()
+
+    # process averages
+    avgTable   = np.zeros(2*nsensors+1)
+    runningAvg = np.zeros(2*nsensors)
+    day = None
+    ntsteps = 0
+    for week in dbFiles:
+        table = readDB(week)
+        for i in range(table.shape[0]):
+            if (day == None) or ((table[i,0].date() - day).days >= ndays):
+                # initialise / reset start day
+                day = table[i,0].date()
+                if ntsteps > 0:
+                    # save running average
+                    avgTable = np.vstack( (avgTable,  np.append(np.array([day]), runningAvg/ntsteps)) )
+                    runningAvg = np.zeros(2*nsensors)
+                    ntsteps = 0
+            else:
+                # add current time stamp to average
+                runningAvg += table[i, 1:].astype(float)
+                ntsteps += 1
+    avgTable = np.delete(avgTable, (0), axis=0)
+    doPlot(avgTable, nback=0, figName='avg_{0:02d}days.png'.format(ndays))
+
+def read_and_plot(plotKind='avg'):
 
     now  = datetime.now()
     year = now.year
     wnum = now.isocalendar()[1]
 
+    yearPath = dbdir + '{:4d}/'.format(year)
+
     if plotKind == '24hrs':
-        fpath = dbdir + '{:4d}/'.format(year)
         fname = 'w{:02d}.csv'.format(wnum)
-        do24hrsPlot(dbdir, fpath + fname)
+        table = readDB(yearPath+fname)
+        doPlot(table, nback=-24*60, figName='24hrs.png')
     elif plotKind == 'avg':
-        doAvgPlot(dbdir, fpath)
+        doAvgPlot(yearPath, ndays=1)
 
 
 if __name__ == '__main__':
