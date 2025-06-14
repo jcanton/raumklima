726854b9a4beee3aafdfbbcc1e9a9bdaf1a1f1e3
Jacopo <jacopo.canton@gmail.com>
2025-06-14 18:42:11 +0200

    cleanup

--- html/make_chart.js
+++ html/make_chart.js
@@ -22,7 +22,6 @@ function getCurrentYearAndWeek() {
     };
 }
 
-
 // Function to fetch CSV data for a specific year and week and update chart
 async function fetchDataAndUpdateChart() {
     const { year, week } = getCurrentYearAndWeek();
@@ -93,7 +92,7 @@ function updateChart(timestamps, sensorData) {
             yaxis: 'y2',
         });
     });
-    
+
     var layout = {
         title: '',
         height: 800,
@@ -159,127 +158,6 @@ function updateChart(timestamps, sensorData) {
     Plotly.newPlot('tempHumChart', data, layout, config);
 
 };
-
-// // Function to update chart with new data
-// function updateChart(timestamps, sensorData) {
-//     let temperatureCtx = document.getElementById('temperatureChart').getContext('2d');
-//     let humidityCtx = document.getElementById('humidityChart').getContext('2d');
-// 
-//     let scales = {
-//         x: {
-//             type: 'time',
-//             time: {
-//                 parser: 'YYYY-MM-DD HH:mm:ss', // Specify the timestamp format
-//                 unit: 'hour' // Adjust as needed based on your data resolution
-//             }
-//         },
-//         y: {
-//             type: 'linear',
-//             display: true,
-//             position: 'left',
-//         },
-//     };
-//     let zoomOptions = {
-//         pan: {
-//             enabled: true,
-//             mode: 'xy',
-//             modifierKey: 'shift',
-//         },
-//         zoom: {
-//             mode: 'xy',
-//             wheel: {
-//                 enabled: true,
-//             },
-//             pinch: {
-//                 enabled: true
-//             },
-//             drag: {
-//                 enabled: true,
-//                 borderColor: 'rgb(54, 162, 235)',
-//                 borderWidth: 1,
-//                 backgroundColor: 'rgba(54, 162, 235, 0.3)'
-//             },
-//         }
-//     };
-//     let plotOpts = {
-//         scales: scales,
-//         plugins: {
-//             zoom: zoomOptions,
-//         }
-//     };
-// 
-//     // Temperature chart
-//     let temperatureChart = new Chart(temperatureCtx, {
-//         type: 'line',
-//         data: {
-//             labels: timestamps,
-//             datasets: Object.keys(sensorData).map((sensorId, index) => ({
-//                 label: `${sensorId} Temperature`,
-//                 data: sensorData[sensorId].temperature,
-//                 borderColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
-//                 backgroundColor: 'rgba(0, 0, 0, 0)',
-//                 tension: 0.1
-//             }))
-//         },
-//         options: plotOpts,
-//     });
-//     temperatureChart.options.plugins.zoom.onZoomComplete = function({ chart }) {
-//         let min = chart.scales.x.min;
-//         let max = chart.scales.x.max;
-//         humidityChart.options.scales.x.min = min;
-//         humidityChart.options.scales.x.max = max;
-//         humidityChart.update();
-//     };
-// 
-//     // Humidity chart
-//     let humidityChart = new Chart(humidityCtx, {
-//         type: 'line',
-//         data: {
-//             labels: timestamps,
-//             datasets: Object.keys(sensorData).map((sensorId, index) => ({
-//                 label: `${sensorId} Humidity`,
-//                 data: sensorData[sensorId].humidity,
-//                 borderColor: `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.5)`,
-//                 backgroundColor: 'rgba(0, 0, 0, 0)',
-//                 tension: 0.1
-//             }))
-//         },
-//         options: plotOpts,
-//     });
-//     humidityChart.options.plugins.zoom.onZoomComplete = function({ chart }) {
-//         let min = chart.scales.x.min;
-//         let max = chart.scales.x.max;
-//         temperatureChart.options.scales.x.min = min;
-//         temperatureChart.options.scales.x.max = max;
-//         temperatureChart.update();
-//     };
-// 
-//     // Add event listeners for the buttons
-//     document.getElementById('toggleZoom').addEventListener('click', () => {
-//         zoomOptions.zoom.drag.enabled = !zoomOptions.zoom.drag.enabled;
-//         temperatureChart.update();
-//         humidityChart.update();
-//     });
-//     document.getElementById('togglePan').addEventListener('click', () => {
-//         zoomOptions.pan.enabled = !zoomOptions.pan.enabled;
-//         temperatureChart.update();
-//         humidityChart.update();
-//     });
-//     document.getElementById('resetZoom').addEventListener('click', () => {
-//         // Reset zoom for temperature chart
-//         temperatureChart.options.scales.x.min = Math.min(...timestamps);
-//         temperatureChart.options.scales.x.max = Math.max(...timestamps);
-//         temperatureChart.options.scales.y.min = Math.min(...sensorData.map(sensor => Math.min(...sensor.temperature)));
-//         temperatureChart.options.scales.y.max = Math.max(...sensorData.map(sensor => Math.max(...sensor.temperature)));
-//         temperatureChart.update();
-//         // Reset zoom for humidity chart
-//         humidityChart.options.scales.x.min = Math.min(...timestamps);
-//         humidityChart.options.scales.x.max = Math.max(...timestamps);
-//         humidityChart.options.scales.y.min = Math.min(...sensorData.map(sensor => Math.min(...sensor.humidity)));
-//         humidityChart.options.scales.y.max = Math.max(...sensorData.map(sensor => Math.max(...sensor.humidity)));
-//         humidityChart.update();
-//     });
-// }
 
 // Call the fetchDataAndUpdateChart function for a specific year and week when the page is loaded
 window.onload = function () {
